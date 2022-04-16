192624671abc9511266d3893ec4ff49810301b22
rjanuszek <rafaela.januszek@gmail.com>
2022-04-16 21:27:29 +0200

    Update

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,6 +5,8 @@ import model.computer.Hdd;
 import model.computer.Ram;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainApp {
 
@@ -76,26 +78,77 @@ public class MainApp {
 
 //        ĆWICZENIA Z KOLEKCJI
 
-        List<Laptop> computerList = new ArrayList<>();
+//        List<Laptop> computerList = new ArrayList<>();
+//
+//        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 100));
+//        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 100));
+//        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 55));
+//        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 70));
+//        computerList.add(new Laptop("Laptop2", "Dell", new Hdd("Dell", 50), new Ram("Dell", 50), 50));
+//        computerList.add(new Laptop("Laptop3", "Dell", new Hdd("Dell", 50), new Ram("Dell", 50), 60));
 
-        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 100));
-        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 100));
-        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 55));
-        computerList.add(new Laptop("Laptop1", "HP", new Hdd("HP", 50), new Ram("HP", 50), 70));
-        computerList.add(new Laptop("Laptop2", "Dell", new Hdd("Dell", 50), new Ram("Dell", 50), 50));
-        computerList.add(new Laptop("Laptop3", "Dell", new Hdd("Dell", 50), new Ram("Dell", 50), 60));
+//        System.out.println("---SET---");
+//        Set<Laptop> computerBattery = new HashSet<>(computerList);
+//        for (Laptop computersBattery : computerBattery) {
+//            System.out.println(computersBattery.getBatteryLevel());
+//        }
+//
+//        System.out.println("---SORT LISTA---");
+//        List<Laptop> computerSort = new ArrayList<>(computerBattery);
+//        Collections.sort(computerSort, Comparator.comparingInt(Laptop::getBatteryLevel));
+//        for (Laptop computersSort : computerSort) {
+//            System.out.println(computersSort.getBatteryLevel());
 
-        System.out.println("---SET---");
-        Set<Laptop> computerBattery = new HashSet<>(computerList);
-        for (Laptop computersBattery : computerBattery) {
-            System.out.println(computersBattery.getBatteryLevel());
-        }
+//        ZADANIE DOMOWE DO MODUŁU 11 STREAM
 
-        System.out.println("---SORT LISTA---");
-        List<Laptop> computerSort = new ArrayList<>(computerBattery);
-        Collections.sort(computerSort, Comparator.comparingInt(Laptop::getBatteryLevel));
-        for (Laptop computersSort : computerSort) {
-            System.out.println(computersSort.getBatteryLevel());
-        }
+        List<Computer> computers = new ArrayList<>();
+        computers.add(new Laptop("MB PRO 1", "PRO 1", new Hdd("HP", 500), new Ram("HP", 128), 100));
+        computers.add(new Laptop("MB PRO 2", "PRO 2", new Hdd("HP", 500), new Ram("HP", 128), 100));
+        computers.add(new Laptop("MB PRO 3", "PRO 3", new Hdd("HP", 256), new Ram("HP", 128), 100));
+        computers.add(new Laptop("MB PRO 4", "PRO 4", new Hdd("HP", 500), new Ram("HP", 128), 100));
+        computers.add(new PC("PC 1", "BBA", new Hdd("HP", 500), new Ram("HP", 128)));
+        computers.add(new PC("PC 2", "AAA", new Hdd("HP", 256), new Ram("HP", 256)));
+        computers.add(new PC("PC 3", "PRO3", new Hdd("HP", 500), new Ram("HP", 128)));
+
+//        1. Zliczyć wszystkie komputery, które mają więcej niż 128 gb ramu
+        System.out.println("---Zadanie 1 ---");
+        long count = computers.stream()
+                .filter(c -> c.getRam().getSize() > 128)
+                .count();
+        System.out.println(count);
+
+//        2. Wyświetlić na konosle wszystkie typy komputerów - map
+        System.out.println("---Zadanie 2 ---");
+        computers.stream()
+                .map(Computer::getType)
+                .forEach(c -> System.out.println(c));
+
+//        3. Wyświetlić komputer, który ma najwięcej ramu
+        System.out.println("---Zadanie 3 ---");
+        Optional<Computer> maxSize = computers.stream()
+                .max(Comparator.comparingInt(c -> c.getRam().getSize()));
+        System.out.println(maxSize);
+
+//        4. Utworzyć nową kolekcję z komputerami, któe mają dysk twardy mniejszy niż 500 gb
+        System.out.println("---Zadanie 4 ---");
+        List<Computer> newCollectList = computers.stream()
+                .filter(c -> c.getHdd().getSize() < 500)
+                .collect(Collectors.toList());
+        newCollectList.stream()
+                .forEach(System.out::println);
+
+//        5. Posortować komputery po nazwie i typie
+        System.out.println("---Zadanie 5 ---");
+        computers.stream()
+                .sorted(Comparator.comparing(Computer::getName).thenComparing(Computer::getType))
+                .forEach(System.out::println);
+
+
+
+
+
+
+
+
     }
 }
